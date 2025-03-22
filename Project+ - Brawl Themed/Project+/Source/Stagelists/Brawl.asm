28aeff3f4f712a715e165566e5c30e4edcea6a1c
ilikepizza107 <90655367+ilikepizza107@users.noreply.github.com>
2025-03-22 09:32:28 -0500

    Caught a dumb, corrected it
    Also renamed some stages, and got some colors figured out with the random select

--- Project+ - Brawl Themed/Project+/Source/Stagelists/Brawl.asm
+++ Project+ - Brawl Themed/Project+/Source/Stagelists/Brawl.asm
@@ -49,7 +49,7 @@ TABLE_1:
 0x1D,  | # PictoChat
 0x2F,  | # Hanenbow_Brawl
 0x1B,  | # Shadow_Moses
-0x1C | # Green_Hill_Zone_Brawl
+0x1C   | # Green_Hill_Zone_Brawl
 
 TABLE_2:
 	byte[10] |
@@ -62,14 +62,14 @@ TABLE_2:
 0x23,  | # Green_Greens
 0x26,  | # Big_Blue
 0x27,  | # Brinstar
-0x28 | # Pokemon_Stadium
+0x28   | # Pokemon_Stadium
 
 TABLE_3:
 TABLE_4:
 TABLE_5:
 
 TABLE_STAGES:
-	half[85] |
+	half[84] |
 0x0101, 0x0202, 0x0303, 0x0404,  | # Battlefield_Brawl, Final_Destination_Brawl, Delfino_Plaza, Luigis_Mansion_Brawl
 0x0505, 0x0606, 0x0707, 0x0808,  | # Mario_Bros, Bowser_Castle, 75_m, Rumble_Falls_Brawl
 0x0909, 0x330A, 0x492C, 0x0C0C,  | # Temple_of_Time, Bridge_of_Eldin, Ceres_Space_Colony, Frigate_Orpheon
@@ -84,14 +84,13 @@ TABLE_STAGES:
 0x4023, 0x4124, 0x4225, 0x251F,  | # Draculas_Castle, Mario_Circuit, Clock_Town, Hanenbow_Brawl
 0x4427, 0x4528, 0x4629, 0x2B34,  | # Dead_Line, Dinosaur_Land, Minecart_Madness, Jungle_Japes
 0x482B, 0x0B0B, 0x4A2D, 0x4B2E,  | # Bell_Tower, Norfair, Cookie_Country, Venus_Lighthouse
-0x4C2F, 0x4D30, 0x4E31, 0x4F3D,  | # Mushroomy_Kingdom, WarioWare, Subspace, Rainbow_Cruise
-0x503E, 0x513F, 0x5240, 0x5341,  | # Poke_Floats, Pirate_Ship, Venom, Battlefield
-0x5442, 0x5543, 0x5644, 0x5745,  | # Final_Destination, Delfino_Secret, Luigis_Mansion, Metal_Cavern
-0x5846, 0x5947, 0x5A48, 0x5B49,  | # Kongo_Jungle, Rumble_Falls, Frigate_Husk, Yoshis_Island
-0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,  | # Saffron_City, Infinite_Glacier, Distant_Planet, Skyworld
-0x604E, 0x614F, 0x6250, 0x6351,  | # Fourside, Green_Hill_Zone, Hannenbow, Dream_Land
-0x6452, 0x6553, 0x6654, 0x6755,  | # Pokemon_Stadium_2, Planet_Zebes, Hyrule_Castle, Peach_Castle
-0x6856 | # Mushroom_Kingdom
+0x4C2F, 0x4D30, 0x4E31, 0x4F3D,  | # Mushroomy_Kingdom, WarioWare, Subspace, Poke_Floats,
+0x503E, 0x513F, 0x5240, 0x5341,  | # Pirate_Ship, Venom, Battlefield, Final Destination
+0x5442, 0x5543, 0x5644, 0x5745,  | # Delfino_Secret, Luigis_Mansion, Metal_Cavern, Kongo_Jungle
+0x5846, 0x5947, 0x5A48, 0x5B49,  | # Rumble_Falls, Frigate_Husk, Yoshis_Island, Saffron_City
+0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,  | # Infinite_Glacier, Distant_Planet, Skyworld, Fourside
+0x604E, 0x614F, 0x6250, 0x6351,  | # Green_Hill_Zone, Hannenbow, Dream_Land, Pokemon_Stadium_2
+0x6452, 0x6553, 0x6654, 0x6755   | # Planet_Zebes, Hyrule_Castle, Peach_Castle, Mushroom_Kingdom
 
 SkipStageTables:
 .RESET
@@ -101,5 +100,5 @@ byte 10 @ $806B92A4 # Page 2
 byte 00 @ $80496002 # Page 3
 byte 00 @ $80496003 # Page 4
 byte 00 @ $80496004 # Page 5
-byte 83 @ $800AF673 # Stage Count
+byte 41 @ $800AF673 # Stage Count
 * E0000000 80008000
--- Project+ - Brawl Themed/Project+/Source/Stagelists/Project+.asm
+++ Project+ - Brawl Themed/Project+/Source/Stagelists/Project+.asm
@@ -25,73 +25,73 @@ TABLE_1:
 0x1E,  | # Sky_Sanctuary
 0x21,  | # Golden_Temple
 0x0A,  | # Ceres_Space_Colony
-0x4A,  | # Distant_Planet
-0x47,  | # Yoshis_Island
+0x49,  | # Distant_Planet
+0x46,  | # Yoshis_Island
 0x15,  | # Wario_Land
 0x18,  | # Fountain_of_Dreams
-0x40,  | # Final_Destination
-0x43,  | # Metal_Cavern
-0x4F,  | # Dream_Land
-0x46,  | # Frigate_Husk
+0x3F,  | # Final_Destination
+0x42,  | # Metal_Cavern
+0x4E,  | # Dream_Land
+0x45,  | # Frigate_Husk
 0x08,  | # Temple_of_Time
-0x4D,  | # Green_Hill_Zone
+0x4C,  | # Green_Hill_Zone
 0x1A,  | # Smashville
-0x3F,  | # Battlefield
-0x50,  | # Pokemon_Stadium_2
-0x41,  | # Delfino_Secret
-0x42 | # Luigis_Mansion
+0x3E,  | # Battlefield
+0x4F,  | # Pokemon_Stadium_2
+0x40,  | # Delfino_Secret
+0x41   | # Luigis_Mansion
 
 TABLE_2:
 	byte[21] |
-0x53,  | # Peach_Castle
-0x44,  | # Kongo_Jungle
-0x45,  | # Rumble_Falls
-0x52,  | # Hyrule_Castle
+0x52,  | # Peach_Castle
+0x43,  | # Kongo_Jungle
+0x44,  | # Rumble_Falls
+0x51,  | # Hyrule_Castle
 0x1F,  | # Temple
 0x0D,  | # Halberd
-0x51,  | # Planet_Zebes
-0x48,  | # Saffron_City
+0x50,  | # Planet_Zebes
+0x47,  | # Saffron_City
 0x10,  | # Spear_Pillar
 0x25,  | # Corneria
 0x0E,  | # Lylat_Cruise
 0x22,  | # Onett
-0x4C,  | # Fourside
-0x49,  | # Infinite_Glacier
+0x4B,  | # Fourside
+0x48,  | # Infinite_Glacier
 0x26,  | # Big_Blue
 0x11,  | # Port_Town
 0x13,  | # Flat_Zone
-0x4B,  | # Skyworld
+0x4A,  | # Skyworld
 0x1B,  | # Shadow_Moses
 0x1D,  | # PictoChat
-0x2B | # Training
+0x2B   | # Training
 
 TABLE_3:
 	byte[19] |
 0x31,  | # Dinosaur_Land
 0x2D,  | # Mario_Circuit
-0x54,  | # Mushroom_Kingdom
-0x3B,  | # Rainbow_Cruise
+0x53,  | # Mushroom_Kingdom
+0x24,  | # Rainbow_Cruise
 0x32,  | # Minecart_Madness
 0x33,  | # Jungle_Japes
 0x36,  | # Cookie_Country
 0x2E,  | # Clock_Town
-0x3D,  | # Pirate_Ship
+0x3C,  | # Pirate_Ship
 0x39,  | # WarioWare
-0x3C,  | # Poke_Floats
+0x3B,  | # Poke_Floats
 0x34,  | # Bell_Tower
 0x35,  | # Norfair
-0x3E,  | # Venom
-0x4E,  | # Hannenbow
+0x3D,  | # Venom
+0x4D,  | # Hannenbow
 0x37,  | # Venus_Lighthouse
 0x2C,  | # Draculas_Castle
 0x30,  | # Dead_Line
-0x3A | # Subspace
+0x3A   | # Subspace
 
 TABLE_4:
 TABLE_5:
 
 TABLE_STAGES:
-	half[85] |
+	half[84] |
 0x0101, 0x0202, 0x0303, 0x0404,  | # Battlefield_Brawl, Final_Destination_Brawl, Delfino_Plaza, Luigis_Mansion_Brawl
 0x0505, 0x0606, 0x0707, 0x0808,  | # Mario_Bros, Bowser_Castle, 75_m, Rumble_Falls_Brawl
 0x0909, 0x330A, 0x492C, 0x0C0C,  | # Temple_of_Time, Bridge_of_Eldin, Ceres_Space_Colony, Frigate_Orpheon
@@ -106,14 +106,13 @@ TABLE_STAGES:
 0x4023, 0x4124, 0x4225, 0x251F,  | # Draculas_Castle, Mario_Circuit, Clock_Town, Hanenbow_Brawl
 0x4427, 0x4528, 0x4629, 0x2B34,  | # Dead_Line, Dinosaur_Land, Minecart_Madness, Jungle_Japes
 0x482B, 0x0B0B, 0x4A2D, 0x4B2E,  | # Bell_Tower, Norfair, Cookie_Country, Venus_Lighthouse
-0x4C2F, 0x4D30, 0x4E31, 0x4F3D,  | # Mushroomy_Kingdom, WarioWare, Subspace, Rainbow_Cruise
-0x503E, 0x513F, 0x5240, 0x5341,  | # Poke_Floats, Pirate_Ship, Venom, Battlefield
-0x5442, 0x5543, 0x5644, 0x5745,  | # Final_Destination, Delfino_Secret, Luigis_Mansion, Metal_Cavern
-0x5846, 0x5947, 0x5A48, 0x5B49,  | # Kongo_Jungle, Rumble_Falls, Frigate_Husk, Yoshis_Island
-0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,  | # Saffron_City, Infinite_Glacier, Distant_Planet, Skyworld
-0x604E, 0x614F, 0x6250, 0x6351,  | # Fourside, Green_Hill_Zone, Hannenbow, Dream_Land
-0x6452, 0x6553, 0x6654, 0x6755,  | # Pokemon_Stadium_2, Planet_Zebes, Hyrule_Castle, Peach_Castle
-0x6856 | # Mushroom_Kingdom
+0x4C2F, 0x4D30, 0x4E31, 0x4F3D,  | # Mushroomy_Kingdom, WarioWare, Subspace, Poke_Floats,
+0x503E, 0x513F, 0x5240, 0x5341,  | # Pirate_Ship, Venom, Battlefield, Final Destination
+0x5442, 0x5543, 0x5644, 0x5745,  | # Delfino_Secret, Luigis_Mansion, Metal_Cavern, Kongo_Jungle
+0x5846, 0x5947, 0x5A48, 0x5B49,  | # Rumble_Falls, Frigate_Husk, Yoshis_Island, Saffron_City
+0x5C4A, 0x5D4B, 0x5E4C, 0x5F4D,  | # Infinite_Glacier, Distant_Planet, Skyworld, Fourside
+0x604E, 0x614F, 0x6250, 0x6351,  | # Green_Hill_Zone, Hannenbow, Dream_Land, Pokemon_Stadium_2
+0x6452, 0x6553, 0x6654, 0x6755   | # Planet_Zebes, Hyrule_Castle, Peach_Castle, Mushroom_Kingdom
 
 SkipStageTables:
 .RESET
@@ -123,5 +122,5 @@ byte 21 @ $806B92A4 # Page 2
 byte 19 @ $80496002 # Page 3
 byte 00 @ $80496003 # Page 4
 byte 00 @ $80496004 # Page 5
-byte 83 @ $800AF673 # Stage Count
+byte 61 @ $800AF673 # Stage Count
 * E0000000 80008000
